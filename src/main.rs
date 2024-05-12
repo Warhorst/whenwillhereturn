c2925082355d71424568a363714d3e93d42b3fcd
Warhorst <46809804+Warhorst@users.noreply.github.com>
2024-05-12 11:21:37 +0200

    Make it run in the web

--- src/main.rs
+++ src/main.rs
@@ -1,45 +1,36 @@
-use std::sync::{Arc, Mutex};
-use std::thread;
-use std::time::Duration;
-
-use chrono::{Local, TimeDelta, TimeZone, Utc};
+use chrono::{Local, TimeZone};
 use eframe::{App, Frame};
 use eframe::epaint::{FontFamily, FontId};
 use egui::{CentralPanel, Context, TextStyle};
 
 #[cfg(not(target_arch = "wasm32"))]
 fn main() -> Result<(), eframe::Error> {
-    let delta_until_return = Arc::new(Mutex::new(TimeDelta::default()));
-
-    let loop_delta_until_return = Arc::clone(&delta_until_return);
-    thread::spawn(move || {
-        let return_date = Local.with_ymd_and_hms(2024, 10, 1, 9, 30, 0).unwrap();
-
-        loop {
-            thread::sleep(Duration::from_secs(1));
-
-            let now = Utc::now();
-            let diff = return_date.signed_duration_since(now);
-
-            let mut delta_until_return = loop_delta_until_return.lock().unwrap();
-            *delta_until_return = diff;
-        }
-    });
-
     eframe::run_native(
         "When will he return?",
         eframe::NativeOptions::default(),
         Box::new(|_| {
-            Box::<WhenWillHeReturnApp>::new(WhenWillHeReturnApp {
-                delta_until_return
-            })
+            Box::<WhenWillHeReturnApp>::new(WhenWillHeReturnApp)
         })
     )
 }
 
-struct WhenWillHeReturnApp {
-    delta_until_return: Arc<Mutex<TimeDelta>>
+#[cfg(target_arch = "wasm32")]
+fn main() {
+    wasm_bindgen_futures::spawn_local(async {
+        eframe::WebRunner::new()
+            .start(
+                "the_canvas_id", // hardcode it
+                eframe::WebOptions::default(),
+                Box::new(|_| {
+                    Box::<WhenWillHeReturnApp>::new(WhenWillHeReturnApp)
+                }),
+            )
+            .await
+            .expect("failed to start eframe");
+    });
 }
+
+struct WhenWillHeReturnApp;
 
 impl App for WhenWillHeReturnApp {
     fn update(&mut self, ctx: &Context, _frame: &mut Frame) {
@@ -49,10 +40,12 @@ impl App for WhenWillHeReturnApp {
                 FontId::new(36.0, FontFamily::Proportional),
             );
 
+            let return_date = Local.with_ymd_and_hms(2024, 10, 1, 9, 30, 0).unwrap();
+            let now = Local::now();
+            let delta = return_date.signed_duration_since(now);
+
             // Yeah, interactive. I am definitely not too lazy to rewrite this in a UI with continuous updates.
             ui.heading("When will he return? This is an interactive counter, so move your mouse to find out!");
-
-            let delta = self.delta_until_return.lock().unwrap();
 
             if delta.num_seconds() < 0 {
                 ui.heading("He returned!");
